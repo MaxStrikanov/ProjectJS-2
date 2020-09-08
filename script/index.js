4bbb148572d139f7e58da40c316cf9391f103c35
Maksim Strikanov <59795160+MaxStrikanov@users.noreply.github.com>
2020-09-08 10:56:26 +0300

    Внес правки

--- script/index.js
+++ script/index.js
@@ -1,14 +1,11 @@
 window.addEventListener('DOMContentLoaded', function(){
   'use strict';
 
- 
   const countTimer = (deadline) => {
 
     let timerHours = document.getElementById( 'timer-hours' );
     let timerMinutes = document.getElementById( 'timer-minutes' );
     let timerSeconds = document.getElementById( 'timer-seconds' );
-
-   
 
     const getTimeRemaining = () => {
 
