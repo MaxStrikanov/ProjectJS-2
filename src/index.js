54f00f2417aa030d4208c4a1af90cd9ff57c99dc
Maksim Strikanov <59795160+MaxStrikanov@users.noreply.github.com>
2020-09-21 21:44:12 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 'use strict';
 
-import "@babel/polyfill";
-import "nodelist-foreach-polyfill";
-import fetch from 'fetch-polyfill';
-import 'formdata-polyfill';
-import elementClosest from 'element-closest';
-elementClosest(window);
-fetch(window);
-
 import countTimer from './modules/countTimer';
 import toggleMenu from './modules/toggleMenu';
 import togglePopup from './modules/togglePopup';
 import tabs from './modules/tabs';
-import addDots from './modules/slider';
-import slider from './modules/slider';
+import {slider, addDots} from './modules/slider';
 import changeImage from './modules/changeImage';
-//import calcValid from './modules/calcValid';
-import calc from './modules/calc';
+import {calc, calcValid} from './modules/calc';
 import sendForm from './modules/sendForm';
 
-countTimer('31 September 2020')
+countTimer('31 September 2020');
 
-toggleMenu();
+toggleMenu(); 
 
 togglePopup();
 
@@ -29,12 +19,12 @@ tabs();
 
 addDots();
 
-slider(1500);
+slider(); 
 
 changeImage();
 
-calcValid();
+calcValid(); 
 
-calc(100);
+calc();
 
 sendForm();
